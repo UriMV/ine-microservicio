b7ea436883375466609705fef1985c1b9cd86840
UriMV23 <isaidvazquez.2309@gmail.com>
2025-05-19 23:42:27 -0600

    UpdateRoutes

--- routes/ine.js
+++ routes/ine.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/ineController');
 
 router.get('/consulta', controller.getByIdOrCurp);
+router.get('/todos', controller.getAll);
 router.post('/insertar', controller.insertAll);
 router.delete('/eliminar/:curp', controller.deleteByCurp);
+router.put('/actualizar/:curp', controller.updateByCurp);
 
 module.exports = router;
